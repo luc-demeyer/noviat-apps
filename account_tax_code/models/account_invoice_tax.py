221749bf805ef91be672c7b213283f1d7a0a13bf
Rudy Valentin <rudy.valentin@eezee-it.com>
2018-06-06 14:39:33 +0200

    [rva/eezeeit-1] Migration of account_tax_code and account_tax_constraints

---
+++ account_tax_code/models/account_invoice_tax.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2009-2018 Noviat.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AccountInvoiceTax(models.Model):
+    _inherit = 'account.invoice.tax'
+
+    tax_code = fields.Char(
+        related='tax_id.code', readonly=True)
