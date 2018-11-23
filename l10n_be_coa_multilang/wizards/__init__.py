693521df03767b2dcffb4e176e6b820e071d0d09
Luc De Meyer <luc.demeyer@noviat.com>
2018-11-23 19:28:01 +0100

    calc init balance when selecting date_rage of type fiscalyear

--- l10n_be_coa_multilang/wizards/__init__.py
+++ l10n_be_coa_multilang/wizards/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import accounting_report
+from . import account_common_report
 from . import l10n_be_coa_multilang_config
 from . import l10n_be_update_be_reportscheme
 from . import l10n_be_vat_common
