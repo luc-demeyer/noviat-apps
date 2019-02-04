a6dd7fa17011d21ce79352e262f78889661e5f0d
Luc De Meyer <luc.demeyer@noviat.com>
2019-02-04 14:50:35 +0100

    add l10n_be_partner_kbo_bce

---
+++ l10n_be_partner_kbo_bce/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2009-2019 Noviat.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Belgium - KBO/BCE numbers',
+    'category': 'Localization',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Noviat,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://odoo-community.org/',
+    'depends': [
+        'partner_identification',
+        'base_vat_sanitized',
+    ],
+    'data': [
+        'data/res_partner_id_category.xml',
+        'views/res_partner.xml',
+    ],
+    'demo': [
+        'demo/res_partner.xml',
+    ],
+    'installable': True,
+}
---
+++ l10n_be_partner_kbo_bce/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_l10n_be_partner_kbo_bce
