077d89d9db62c644efd255fcdf45d4ba1533fc5c
Luc De Meyer <luc.demeyer@noviat.com>
2019-02-12 16:09:19 +0100

    [MIG] account_move_line_import: Migration to 11.0

---
+++ account_move_line_import/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2009-2019 Noviat
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Move Line Import',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Noviat,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/account-financial-tools',
+    'category': 'Accounting & Finance',
+    'summary': 'Import Accounting Entries',
+    'depends': ['account'],
+    'data': [
+        'views/account_move.xml',
+        'wizard/import_move_line_wizard.xml',
+    ],
+    'installable': True,
+}
---
+++ account_move_line_import/wizard/__init__.py
@@ -0,0 +1,1 @@
+from . import import_move_line_wizard
