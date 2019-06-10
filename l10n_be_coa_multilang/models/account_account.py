1c611da074da57c4462d4a039fc6ef0530d12272
luc.demeyer@noviat.com <luc.demeyer@noviat.com>
2019-06-10 17:57:14 +0200

    openerp -> odoo

--- l10n_be_coa_multilang/models/account_account.py
+++ l10n_be_coa_multilang/models/account_account.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models, _
-from openerp.exceptions import UserError
+from odoo.exceptions import UserError
 
 
 class AccountAccount(models.Model):
