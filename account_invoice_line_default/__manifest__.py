def2ad5b71219c1f9d667beb534e56fc8b8602e8
Luc De Meyer <luc.demeyer@noviat.com>
2019-11-12 20:46:55 +0100

    [11.0][ADD]account_invoice_line_default

---
+++ account_invoice_line_default/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2009-2019 Noviat.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Invoice Line Defaults',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Noviat',
+    'website': 'http://www.noviat.com',
+    'category': 'Accounting & Finance',
+    'complexity': 'normal',
+    'summary': 'Account Invoice Line Defaults',
+    'data': [
+        'views/account_invoice.xml',
+        'views/res_partner.xml',
+    ],
+    'depends': [
+        'account',
+    ],
+    'installable': True
+}
