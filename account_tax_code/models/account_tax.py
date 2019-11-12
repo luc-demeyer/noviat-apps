2d6589137cd1308207630c186de3e337027f73f0
Luc De Meyer <luc.demeyer@noviat.com>
2019-11-12 20:51:05 +0100

    [11.0][IMP]copy on tax code

--- account_tax_code/models/account_tax.py
+++ account_tax_code/models/account_tax.py
@@ -1,7 +1,7 @@
-# Copyright 2009-2018 Noviat.
+# Copyright 2009-2019 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import _, api, fields, models
 
 
 class AccountTax(models.Model):
@@ -13,3 +13,9 @@ class AccountTax(models.Model):
         ('code_company_uniq', 'unique (code,company_id)',
          'The code of the Tax must be unique per company !')
     ]
+
+    @api.one
+    @api.returns('self', lambda value: value.id)
+    def copy(self, default=None):
+        default = dict(default or {}, code=_("%s (Copy)") % (self.code or ''))
+        return super().copy(default=default)
